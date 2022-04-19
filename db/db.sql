b5e6a0101258e47f1a30b16ce3dc4e52fd696427
MJB <matt.berti13@gmail.com>
2022-04-19 15:31:29 -0500

    created table from candidates in election db

---
+++ db/db.sql
@@ -0,0 +1,7 @@
+-- attempts to drop previously existing database, enables creation of new database
+DROP DATABASE IF EXISTS election;
+
+/*Create election database to store data*/
+CREATE DATABASE election;
+-- Selects into election database to add/remove data
+USE election;
---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+-- Creates tables we have created in MySQL CLI
+CREATE TABLE candidates(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    industry_connected BOOLEAN NOT NULL
+);
