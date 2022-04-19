65db1afa1bb0447ff7c8584b00ece81afff46bb9
MJB <matt.berti13@gmail.com>
2022-04-19 16:58:54 -0500

    queries for creating, deleting, and finding candidates

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 
 //imports express into server file
 const express = require('express');
+const res = require('express/lib/response');
 
 
 //sets the port designation for hosting on server
@@ -37,11 +38,49 @@ const db = mysql.createConnection(
 //     });
 // });
 
+
+// ****QUERIES****
 //Runs SQL Query and executes callback with all resulting rows that match query
 //If there are no errors, err is null, THIS IS KEY!!!
-db.query(`SELECT * FROM candidates`, (err, rows) =>{
-    console.log(rows);
+// db.query(`SELECT * FROM candidates`, (err, rows) =>{
+//     console.log(rows);
+// });
+
+//GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = ?`, 1, (err, row) =>{
+//     if(err){
+//         console.log(err);
+//     }
+
+//     console.log(row);
+
+// });
+
+//Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) =>{
+//     if(err){
+//         console.log(err);
+//     }
+
+//     console.log(result);
+
+// });
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+
+const params = [1, 'Ronald', 'Firman', 1];
+
+db.query(sql,params, (err, result) =>{
+    if(err){
+        console.log(err)
+    }
+
+    console.log(result);
+
 });
+
 
 //Default Response for any other request(Not Found)
 //CATCHALL ROUTE
