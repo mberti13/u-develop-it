69ab200ec9bbeddd8725f838b15b2e6710b2bbe1
MJB <matt.berti13@gmail.com>
2022-04-19 16:40:43 -0500

    created initial query to test connection to mysql database

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+//imports mysql2
+const mysql = require('mysql2');
+
 //imports express into server file
 const express = require('express');
 
@@ -12,6 +15,20 @@ const app = express();
 app.use(express.urlencoded({ extended:false }));
 app.use(express.json());
 
+//Connects application to MySQL Database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //MySQL Username
+        user: 'root',
+        //MySQL Password
+        password: 'BIwuHz6fUc+3k6@oFDQMV1wECxS%D=',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+
 
 //TEST GET ROUTE
 // app.get('/', (req,res) =>{
@@ -19,6 +36,12 @@ app.use(express.json());
 //         message: 'Hello World'
 //     });
 // });
+
+//Runs SQL Query and executes callback with all resulting rows that match query
+//If there are no errors, err is null, THIS IS KEY!!!
+db.query(`SELECT * FROM candidates`, (err, rows) =>{
+    console.log(rows);
+});
 
 //Default Response for any other request(Not Found)
 //CATCHALL ROUTE
